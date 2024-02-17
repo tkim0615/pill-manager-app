72bdd194837ea2cfa06f876681e561c96c2a270b
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-17 13:23:06 -0500

    added all models

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response, session
 from flask_restful import Resource
 
 # Local imports
@@ -11,23 +11,87 @@ from config import app, db, api
 # Add your model imports
 from models import User
 
-
 # Views go here!
+app.secret_key = b'\x8c\xbb\xa9\xa5\xf0\x8c01c\xc1\xec\xa4\x9fs\xbf=\x83(\xd5Z8\xa5A\xd3'
 
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
 
-
+class Users(Resource):
+    def get(self):  
+        users = [User.to_dict() for user in User.query.all()]
+        return make_response(users, 200)
+    
+    def post(self):
+        try:
+            data = request.get_json()
+            user = User(
+                name= data['name'],
+                username= data['username'],
+            )
+            user.password_hash = data['password'] ######################3
+            db.session.add(user)
+            db.session.commit()
+            return make_response(user.to_dict(), 201)
+        except ValueError:
+            return make_response({'error': 'Failed to add new user, try again!'}, 400)  
 
 class UserById(Resource):
     def get(self, id):
-        user = User.query.filter()
-        pass
+        user = User.query.filter(User.id == id).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({'error': 'user not found'},404)
+    def patch(self, id):
+        user = User.query.filter(User.id == id).first()
+        if user:
+            try:
+                data = request.get_json()
+                for attr in data:
+                    setattr(user, attr, data[attr])
+                db.session.commit()
+                return make_response(user.to_dict(), 202)
+            except ValueError:
+                return make_response({'error': 'Failed to edit user'}, 400)
+            
+api.add_resource(UserById, '/users/<int:id>')
+api.add_resource(Users, '/user')
 
-api.add_resource(User, '/user/<int:id>')
 
 
+#authentification
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {'message': '401: Not Authorized'}, 401
+
+class Login(Resource):
+    def post(self):
+        try:
+            username = request.get_json()['username']
+            password = request.get_json()['password']
+
+            user = User.query.filter(User.username == request.get_json()['username']).first()
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return user.to_dict(), 200
+            return {'error': 'Invalid username or password'}, 401
+            
+        except ValueError:
+            return make_response({'error': 'Login failed'}, 400)     
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204 
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
 
 
 
--- server/models.py
+++ server/models.py
@@ -1,8 +1,8 @@
-
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
+from datetime import date, datetime, time
 from config import db, metadata, bcrypt
 
 
@@ -18,18 +18,18 @@ class User(db.Model, SerializerMixin):
     _password_hash = db.Column(db.String)
 
     # Add relationship
-    # classes = db.relationship('Class', back_populates='dog', cascade = 'all, delete')
-    
+    side_effects = db.relationship('Side_effect', backref='user', cascade = 'all, delete')
+    dosage_histories = db.relationship('Dosage_history', backref='user', cascade = 'all, delete')
+
     # Add serialization rules
-    # serialize_rules=('-classes.dog',)
+    serialize_rules=('-side_effects.user',)
 
     # Add validation
-    # @validates('name', 'owner', 'breed')
-    # def validate_dog(self, key, value):
-    #     if not value:
-    #         raise ValueError('Name, Owner, and breed must exist!')
-    #     return value
-
+    @validates('name', 'username', 'password_hash')
+    def validate_user(self, key, value):
+        if not value:
+            raise ValueError('Name, username, and password must exist!')
+        return value
 
     @hybrid_property
     def password_hash(self):    
@@ -49,3 +49,91 @@ class User(db.Model, SerializerMixin):
     def __repr__(self):
         return f'<User {self.id}: {self.name} username:{self.username}>'
     
+
+class Prescription(db.Model, SerializerMixin):
+    __tablename__ = 'prescriptions'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    direction = db.Column(db.String)
+    start_date = db.Column(db.Date)
+    end_date = db.Column(db.Date)
+    completed = db.Column(db.Boolean)
+
+        # Add relationship
+    side_effects = db.relationship('Side_effect', backref='prescription', cascade = 'all, delete')
+    dosage_histories = db.relationship('Dosage_history', backref='prescription', cascade = 'all, delete')
+
+        # Add serialization rules
+    serialize_rules=('-side_effects.prescription',)
+    
+        #validations
+    @validates('start_date', 'end_date')
+    def validate_rx_date(self, key, value):
+            if isinstance(value, str): 
+                value = datetime.strptime(value, '%Y-%m-%d').date()
+            elif isinstance(value, datetime):
+                value = value.date()
+            elif not isinstance(value, date):
+                raise ValueError(f"ERROR: {key} must be of type datetime.date")
+            return value
+
+
+    def __repr__(self):
+        return f'<Med : {self.name} direction:{self.direction} start: {self.start_date} end: self.end_date>'
+    
+class Side_effect(db.Model, SerializerMixin):
+    __tablename__ = 'side_effects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    symptom = db.Column(db.String)
+    
+    # Add relationship 
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    prescription_id = db.Column(db.Integer, db.ForeignKey('prescriptions.id'))
+
+    
+     # Add serialization rules
+    serialize_rules=('-user.side_effects', '-prescription.side_effects')
+    
+        #validations
+
+class Dosage_history(db.Model, SerializerMixin):
+    __tablename__ = 'dosage_histories'
+
+    id = db.Column(db.Integer, primary_key=True)
+    time_taken = db.Column(db.Time)
+    date_taken= db.Column(db.Date)
+
+    # Add relationship
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    prescription_id = db.Column(db.Integer, db.ForeignKey('prescriptions.id'))
+
+        # Add serialization rules
+    serialize_rules=('-user.dosage_histories', '-prescription.dosage_histories')
+
+        #validations
+
+
+    @validates('time_taken', 'date_taken')
+    def validate_dosage(self, key, value):
+        if key == 'time_taken':
+            try:
+                # Parse the time string to a datetime object
+                time_object = datetime.strptime(value, '%H:%M').time()
+                return time_object
+            except ValueError:
+                raise ValueError("Invalid time format. Please use HH:MM.")
+
+        elif key == 'date_taken':
+            if isinstance(value, str): 
+                try:
+                    value = datetime.strptime(value, '%Y-%m-%d').date()
+                except ValueError:
+                    raise ValueError("Invalid date format. Please use YYYY-MM-DD.")
+            elif not isinstance(value, date):
+                raise ValueError(f"ERROR: {key} must be of type datetime.date")
+        return value
+
+
+
