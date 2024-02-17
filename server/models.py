d76d5fbb2e216d769675a71be33e752d2af2c4c0
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-16 22:59:39 -0500

    add user model and seed data

--- server/models.py
+++ server/models.py
@@ -1,6 +1,51 @@
+
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
+from config import db, metadata, bcrypt
 
-from config import db
+
+from config import db, metadata
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+
+    # Add relationship
+    # classes = db.relationship('Class', back_populates='dog', cascade = 'all, delete')
+    
+    # Add serialization rules
+    # serialize_rules=('-classes.dog',)
+
+    # Add validation
+    # @validates('name', 'owner', 'breed')
+    # def validate_dog(self, key, value):
+    #     if not value:
+    #         raise ValueError('Name, Owner, and breed must exist!')
+    #     return value
+
+
+    @hybrid_property
+    def password_hash(self):    
+        raise Exception('Password hashes may not be viewed.')
+#removed decode, encoding
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password)
+        self._password_hash = password_hash
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password)
+
+
+    def __repr__(self):
+        return f'<User {self.id}: {self.name} username:{self.username}>'
+    
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,43 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
+fake = Faker()
+
+
+def create_users():
+    users = []
+    for _ in range(5):
+        u = User(
+            name=fake.name(),
+            username = fake.first_name()
+        )
+        u.password_hash = 'abc1'
+        users.append(u)
+
+    return users
 
 if __name__ == '__main__':
-    fake = Faker()
     with app.app_context():
-        print("Starting seed...")
+        print("Clearing db...")
+        User.query.delete()
+  
+
+        print("Seeding users...")
+        users =create_users()
+        db.session.add_all(users)
+        db.session.commit()
+
+        # print("Seeding trainers...")
+        # trainers = create_trainers()
+        # db.session.add_all(trainers)
+        # db.session.commit()
+
+        # print("Seeding classes...")
+        # classes = create_classes(dogs, trainers)
+        # db.session.add_all(classes)
+        # db.session.commit()
+
+        print("Done seeding!")
         # Seed code goes here!
+
