3db5b12e3e9ad1ca578af6916391fbfca092c599
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-16 23:08:07 -0500

    erd path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return <h1>Project Clients</h1>;
 }
 
 export default App;
