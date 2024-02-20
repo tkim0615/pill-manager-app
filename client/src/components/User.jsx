10b0b40359bdf0742879fd4b2dfce9c711eca113
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-20 14:19:14 -0500

    user patch done

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -3,11 +3,17 @@ import { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
 
 
 
 const User = ({user}) => {
   const [fetchedUser, setFetchedUser] = useState({})
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
   const { id } = useParams();
 
     
@@ -24,6 +30,25 @@ const User = ({user}) => {
   }, [id]);
 
 
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    fetch(`/users/${user.id}`,{
+      
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({username, password})
+    })
+      .then((r) => r.json())
+      .then((editedUser) => {
+        setFetchedUser(editedUser)
+        setUsername('')
+        setPassword('')
+      })
+      .catch((error) => console.error('Error:', error));
+  };
+  
+
+
   return (
     <Container className="mt-5">
       <Card>
@@ -37,9 +62,29 @@ const User = ({user}) => {
           </Card.Text>          
         </Card.Body>
       </Card>
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Label>Edit account info</Form.Label>
+
+                <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Label>New Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter new username" value={username}
+                        onChange={e => setUsername(e.target.value)}/>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>New Password</Form.Label>
+                <Form.Control type="password" placeholder="Enter new password" value={password}
+                    onChange={e => setPassword(e.target.value)}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                Submit
+                </Button>
+
+        </Form>
     </Container>
   )
-}
+  }
 
 export default User
 
