dfbf2b2c15ed9e76b74f8483684620f6e3520340
tyler kim <yoonsoo.kim11@gmail.com>
2024-02-22 16:01:22 -0500

    fixed dh fetch issue in app.py

--- server/models.py
+++ server/models.py
@@ -37,12 +37,12 @@ class User(db.Model, SerializerMixin):
     @password_hash.setter
     def password_hash(self, password):
         password_hash = bcrypt.generate_password_hash(
-            password)
-        self._password_hash = password_hash
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
 
     def authenticate(self, password):
         return bcrypt.check_password_hash(
-            self._password_hash, password)
+            self._password_hash, password.encode('utf-8'))
 
     def __repr__(self):
         return f'<User {self.id}: {self.name} username:{self.username}>'
